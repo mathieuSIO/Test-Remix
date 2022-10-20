01e8fc81a4d900854d79ad671e2ab6d34797c802
Remix Workspace <>
2022-10-21 00:04:06 +0200

    vidéo 5 : Mapping

--- contracts/test.sol
+++ contracts/test.sol
@@ -1,31 +1,19 @@
 pragma solidity 0.8.7;
 
-//finney, Gwei et Wei sont des sous unité d'ether
+//En solidity priviligier les maps aux tableaux car les tableaux coutent cher en gaz
 
-//Contrat de test de la vidéo Envoyer de l'ether sur un contrat msg.sender, msg.value
+//Vidéo sur le mapping
 contract test {
+    mapping(address => uint) Balances;
 
-    address lastPerson; //La derniere  personne qui a envoyé de l'argent sur le smart contract
-    uint balance; //Montant envoyé sur le smart contract
-
-    function getLastPeron() public view returns(address){
-        return lastPerson;
+    //On récupere la balance d'un compte donné sur notre contrat
+    function getBalance(address _monAddress) public view returns(uint _balance) {
+        return Balances[_monAddress];
     }
 
-    function getBalance() public view returns(uint) {
-        return balance;
-    }
-
-
-    //Permet d'envoyer de l'argent sur le smart contract
-    //Il ne peut y avoir qu'une seul fonction recieve par contrat 
-    //payable indique qu'il va y avoir une variation d'argent
-    receive() external payable {
-        //Permet de recupérer la derniere personne qui a envoyé de l'argent
-        lastPerson = msg.sender;
-        
-        //Permet de récupérer le montant en Wei envoyé par l'user
-        balance = msg.value;
+    receive() external payable{
+        //Nous permet d'associer un montant à une adresse
+        Balances[msg.sender] = msg.value;
     }
 
 }
